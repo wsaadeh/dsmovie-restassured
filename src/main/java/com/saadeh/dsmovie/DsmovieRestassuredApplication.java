4c4bc0578e6974fbd4b6e34648f5347457a8f821
Wilson Saadeh <wilson_saadeh@hotmail.com>
2025-10-28 09:08:54 -0300

    project created

---
+++ src/main/java/com/saadeh/dsmovie/DsmovieRestassuredApplication.java
@@ -0,0 +1,13 @@
+package com.saadeh.dsmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmovieRestassuredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmovieRestassuredApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saadeh/dsmovie/DsmovieRestassuredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saadeh.dsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieRestassuredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
